225180b04f1a708b8c17e4d0c8c066cfb90243f7
otabek-a <otabekabdurasulov245@gmail.com>
2024-11-07 17:48:33 +0500

    test

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from user import User
-person=User('otabek','Abdurasulov',17,'udent')
+person=User('otabek','Abdurasulov',17,'student')
 
 def test_1():
     assert person.name=='otabek'
     assert person.surname=='Abdurasulov'
-    
+    assert person.age==17
+    assert person.accupation=='student'
 test_1()
